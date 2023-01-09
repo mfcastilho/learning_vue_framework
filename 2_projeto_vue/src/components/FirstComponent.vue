52ccd1d774d949b120d93df1f3e4b968d6d2cb12
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-08 21:58:15 -0300

    learning about how we create a component

---
+++ 2_projeto_vue/src/components/FirstComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <FirstComponent />
+</template>
+
+<script>
+  import FirstComponent from './components/FirstComponent.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      FirstComponent
+    }
+  }
+</script>
